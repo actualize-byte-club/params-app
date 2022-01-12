d7ebac60b9a2dfe7cd5757b14d6c8d07df5afaa7
dani <dani.zaghian@gmail.com>
2022-01-12 11:41:35 -0800

    adds query parameter example

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,2 +1,12 @@
 class ParamsExamplesController < ApplicationController
+
+  def query_params
+    my_message = params["message"]
+    second_message = params["second_message"]
+    render json: {
+      message: "The message is #{my_message}",
+      second_message: "The second message is #{second_message}"
+    }
+  end
+
 end
